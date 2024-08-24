792c09eb68264d3097de790de1b4841f558de950
bruno <bweisz@eryxsoluciones.com.ar>
2024-08-24 19:32:06 -0300

    Agregados los cambios de Lolo

---
+++ circuit/nerf_miner.js
@@ -0,0 +1,29 @@
+{
+  const ORIG_ROWS = 8;
+  const ORIG_COLS = 10;
+  const CROPPED_ROWS = 3;
+  const CROPPED_COLS = 4;
+  const OFFSET_ROWS = 1;
+  const OFFSET_COLS = 5;
+  const HASH = "0x2ae0b9ceacd6d96c3c051341c46c5a14585d714e5a51f0466fb67529cd373a60";
+}
+
+{
+  const ORIG_ROWS = 8;
+  const ORIG_COLS = 50;
+  const CROPPED_ROWS = 3;
+  const CROPPED_COLS = 20;
+  const OFFSET_ROWS = 1;
+  const OFFSET_COLS = 5;
+  const HASH = "0x14dd6c7375fb1ebcfbc6d15bbc903b6fc9460ff180abfe85a3d1f8077be21239";
+}
+
+{
+  const ORIG_ROWS = 100;
+  const ORIG_COLS = 100;
+  const CROPPED_ROWS = 20;
+  const CROPPED_COLS = 20;
+  const OFFSET_ROWS = 1;
+  const OFFSET_COLS = 5;
+  const HASH = "0x0294a8c8c4dfbf6e545d7c80c9f5ffc38666c12f5150493791730bd12e323f2e";
+}
